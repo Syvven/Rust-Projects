2e0db8ad4866b3703db79295e42505f576a48742
hend0800 <hend0800@umn.edu>
2022-05-15 16:00:37 -0500

    finished minitar program and comments

--- minitar/src/main.rs
+++ minitar/src/main.rs
@@ -1,16 +1,18 @@
 use std::env;
 use std::process;
 
+// bring the crate containing most logic into scope
 use minitar::Config;
 
 fn main() {
+    // initialize the config struct based on the input arguments
     let config = Config::new(env::args()).unwrap_or_else(|err| {
         eprintln!("{}", err);
         process::exit(1);
     });
 
-    println!("{:?}", config);
-
+    // run the main function 
+    // propogated errors are handled here
     if let Err(e) = minitar::minitar_main(config) {
         eprintln!("Application error: {}", e);
         process::exit(1);
